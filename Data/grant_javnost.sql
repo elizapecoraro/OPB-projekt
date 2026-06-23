646f83b40faf0d7450b8bbd02eed39bdbf35811f
elizapecoraro <eliza.pecorar@gmail.com>
2026-06-23 13:48:14 +0200

    popravljen dostop do baze za uporabnika javnost

--- Data/grant_javnost.sql
+++ Data/grant_javnost.sql
@@ -1,16 +1,11 @@
--- Ukaze izvede lastnik baze.
--- Aplikacija je bralna, zato uporabnik javnost potrebuje pravico SELECT.
+-- Te ukaze izvede lastnik baze.
+-- Uporabniku javnost omogočijo branje podatkov aplikacije.
 
 GRANT CONNECT ON DATABASE sem2026_kajabl TO javnost;
+
 GRANT USAGE ON SCHEMA public TO javnost;
 
-GRANT SELECT ON TABLE
-    lokacija,
-    restavracija,
-    kuhinja,
-    restavracija_kuhinja,
-    delovni_cas
-TO javnost;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO javnost;
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA public
 GRANT SELECT ON TABLES TO javnost;
